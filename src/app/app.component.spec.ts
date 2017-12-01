d490bea18131a139a9e50caceb2724b1ef42e4b0
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-01 16:42:45 -0200

    criado unitário básico para verificar se o component App está sendo criado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
+import { ComponentsModule } from './components/components.module';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        ComponentsModule
+      ]
     }).compileComponents();
   }));
-  // it('Devera criar o App Component', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // }));
+  it('Devera criar o App Component', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 });
