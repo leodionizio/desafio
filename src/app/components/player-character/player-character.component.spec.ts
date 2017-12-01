d2786221d3c4d907f408683cfc95bf30b170f27e
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-01 16:44:50 -0200

    criado testes unitários do componente player-character

--- src/app/components/player-character/player-character.component.spec.ts
+++ src/app/components/player-character/player-character.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayerCharacterComponent } from './player-character.component';
+import { Player } from '../../models/player.model';
 
 describe('PlayerCharacterComponent', () => {
   let component: PlayerCharacterComponent;
@@ -8,18 +9,49 @@ describe('PlayerCharacterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerCharacterComponent ]
+      declarations: [PlayerCharacterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayerCharacterComponent);
+    fixture.componentInstance.player = new Player(
+      'playerOne',
+      '',
+      100,
+      'life-bar-player-one',
+      'assets/player/player1.png',
+      'assets/player/player1-behit.png',
+    );
+    fixture.componentInstance.playerHitting = new Player(
+      'playerTwo',
+      '',
+      0,
+      'life-bar-player-two',
+      'assets/player/player2.png',
+      'assets/player/player2-behit.png',
+    );
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Devera criar o componente PlayerCharacter', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Smoke Test Components', () => {
+    
+    it('Devera possuir uma variavel player do tipo `Player`', () => {
+      expect(component.player).toBeTruthy();
+      expect(typeof (component.player)).toBeTruthy('Player');      
+    });
+
+    it('Devera possuir uma variavel playerHitting do tipo `Player`', () => {
+      expect(component.playerHitting).toBeTruthy();
+      expect(typeof (component.playerHitting)).toBeTruthy('Player');
+    });
+
+  })
+
 });
