d1f5c123d1bf69baa7ff1e0a354d0c4e53573f36
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-01 16:44:57 -0200

    criado testes unitários do componente player-life

--- src/app/components/player-life/player-life.component.spec.ts
+++ src/app/components/player-life/player-life.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayerLifeComponent } from './player-life.component';
+import { Player } from '../../models/player.model';
 
 describe('PlayerLifeComponent', () => {
   let component: PlayerLifeComponent;
@@ -8,18 +9,42 @@ describe('PlayerLifeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerLifeComponent ]
+      declarations: [PlayerLifeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayerLifeComponent);
+    fixture.componentInstance.player = new Player(
+      'playerOne',
+      '',
+      100,
+      'life-bar-player-one',
+      'assets/player/player1.png',
+      'assets/player/player1-behit.png',
+    );
+    fixture.componentInstance.playerLife = 100;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Devera criar o componente PlayerLife', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Smoke Test Components', () => {
+
+    it('Devera possuir uma variavel player do tipo `Player`', () => {
+      expect(component.player).toBeTruthy();
+      expect(typeof (component.player)).toBeTruthy('Player');
+    });
+
+    it('Devera possuir uma variavel playerLife contendo um valor numerico', () => {
+      expect(component.playerLife).toBeTruthy();
+      expect(typeof (component.playerLife)).toBeTruthy('number');
+    });
+
+  })
+
 });
