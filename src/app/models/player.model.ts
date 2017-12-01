26a4b661dc8314eecb5d0621506aacfa85ade320
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-01 12:36:17 -0200

    criado modelo do jogador

---
+++ src/app/models/player.model.ts
@@ -0,0 +1,11 @@
+export class Player {
+
+    constructor(
+        public idName: string,
+        public name: string,
+        public life: number,
+        public classStyle: string,
+        public imgHit: string,
+        public imgBeHit: string
+    ) { }
+}
